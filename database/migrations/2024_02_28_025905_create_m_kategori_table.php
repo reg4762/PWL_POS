0467de2d2126e7cdd97f26b57162855425bfccfc
Regita Delya Putri <regitadelyap@gmail.com>
2024-02-28 11:28:25 +0700

    third commit

--- database/migrations/2024_02_28_025905_create_m_kategori_table.php
+++ database/migrations/2024_02_28_025905_create_m_kategori_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('m_level', function (Blueprint $table) {
-            $table->id();
+        Schema::create('m_kategori', function (Blueprint $table) {
+            $table->id('kategori_id');
+            $table->string('kategori_kode', 10)->unique();
+            $table->string('kategori_nama', 100);
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('m_level');
+        Schema::dropIfExists('m_kategori');
     }
 };
