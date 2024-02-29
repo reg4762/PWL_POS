0ceb2e8a4b30d4c06071eb7f9612f1dcbecd3c8f
Regita Delya Putri <regitadelyap@gmail.com>
2024-03-01 00:03:48 +0700

    sixth commit

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,11 @@
 
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\KategoriController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
