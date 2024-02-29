7d03c19f086c9bfad93b69ac7468221e350d0ebf
Regita Delya Putri <regitadelyap@gmail.com>
2024-03-01 01:00:12 +0700

    seventh commit

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,4 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
