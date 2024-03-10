089e655e42a4df56f871a802fa912842ae4b618e
Regita Delya Putri <regitadelyap@gmail.com>
2024-03-10 21:47:14 +0700

    p4 commit ke-1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,15 +10,25 @@ class UserController extends Controller
 {
     public function index()
     {
+        // // tambah data user dengan Eloquent Model
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        // ];
+        // UserModel::insert($data); // tambahkan data ke tabel m_user
 
-    // tambah data user dengan Eloquent Model
-    $data = [
-        'nama' => 'Pelanggan Pertama'
-    ];
-    UserModel::where('username', 'customer-1')->update($data); //update data user
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
 
-    // coba akses model UserModel
-    $user = UserModel::all(); //ambil semua data dari tabel m_user
-    return view('user', ['data' => $user]);
+        // coba akses model UserModel
+        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
     }
 }
