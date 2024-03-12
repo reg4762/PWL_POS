0e317ed77ea15ba3e911f010e176da648f11b3cc
Regita Delya Putri <regitadelyap@gmail.com>
2024-03-13 05:04:14 +0700

    p4 commit ke-8

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -10,11 +10,13 @@
     <a href = "/user/tambah">Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
-            <th>Aksi</th>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+            <td>Kode Level</td>
+            <td>Nama Level</td>
+            <td>Aksi</td>
         </tr>
         @foreach ($data as $d)
             <tr>
@@ -22,6 +24,8 @@
                 <td>{{ $d->username }}</td>
                 <td>{{ $d->nama }}</td>
                 <td>{{ $d->level_id }}</td>
+                <td>{{ $d->level->level_kode }}</td> 
+                <td>{{ $d->level->level_nama }}</td> 
                 <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a> 
                     | <a href="/user/hapus/{{ $d->user_id }}">Hapus</a></td>
             </tr>
