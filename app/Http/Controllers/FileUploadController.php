aad3e37ec09f8f33b6b86d5670796d5c791d2da4
Regita Delya Putri <regitadelyap@gmail.com>
2024-05-26 21:45:34 +0700

    p12 commit ke-7

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -12,16 +12,22 @@ class FileUploadController extends Controller
         return view('file-upload');
     }
 
-    public function prosesFileUpload(Request $request){
-        $request->validate([
-            'berkas'=>'required|file|image|max:500',]);
-            $extfile=$request->berkas->getClientOriginalName();
-            $namaFile = 'web-'.time().'.'.$extfile;
-            $path = $request->berkas->storeAs('public',$namaFile);
+    public function prosesFileUpload(Request $request)
+    {
 
-            $pathBaru=asset('storage/'.$namaFile);
-            echo "proses upload berhasil, data disimpan pada: $path";
-            echo "<br>";
-            echo "Tampilkan link: <a href='$pathBaru'>$pathBaru</a>";
+        $request->validate([
+            'berkas' => 'required|file|image|max:500',
+        ]);
+        $extFile = $request->berkas->getClientOriginalName();
+        $namaFile = "web-" . time() . "." . $extFile;
+
+        $path = $request->berkas->move('gambar', $namaFile);
+        $path = str_replace("\\", "//", $path);
+        echo "Variabel path bersih: $path <br>";
+
+        $pathBaru = asset('gambar/' . $namaFile);
+        echo "proses upload berhasil, data disimpan pada: $path";
+        echo "<br>";
+        echo "Tampilkan link: <a href='$pathBaru'>$pathBaru</a>";
     }
 }
