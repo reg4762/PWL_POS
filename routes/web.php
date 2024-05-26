800193c22c3ccb6383d2b7ca726d6fb9bcba5fa1
Regita Delya Putri <regitadelyap@gmail.com>
2024-05-26 20:50:09 +0700

    p12 commit ke-1

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ use App\Http\Controllers\TransaksiPenjualanController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ManagerController;
+use App\Http\Controllers\FileUploadController;
+
 
 
 
@@ -165,3 +167,6 @@ Route::group(['middleware' => ['auth']], function () {
         Route::resource('manager', ManagerController::class);
     });
 });
+
+Route::get('/file-upload', [FileUploadController::class, 'fileUpload']);
+Route::post('/file-upload', [FileUploadController::class, 'prosesfileUpload']);
