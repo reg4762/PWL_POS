a33309c677413db551fe07a904dc0cd76df6d633
Regita Delya Putri <regitadelyap@gmail.com>
2024-05-26 20:56:42 +0700

    p12 commit ke-2

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -16,21 +16,8 @@ class FileUploadController extends Controller
         // dump($request->berkas);
         // dump($request->file('file'));
         // return "Pemrosesan file upload di sini";
-        if($request->hasFile('berkas')){
-            echo "path(): ".$request->berkas->path();
-            echo "<br>";
-            echo "extension(): ".$request->berkas->extension();
-            echo "<br>";
-            echo "getClientOriginalExtension(): ".$request->berkas->getClientOriginalExtension();
-            echo "<br>";
-            echo "getMimeType(): ".$request->berkas->getMimeType();
-            echo "<br>";
-            echo "getClientOriginalName(): ".$request->berkas->getClientOriginalName();
-            echo "<br>";
-            echo "getSize(): ".$request->berkas->getSize();
-        }
-        else {
-            echo "Tidak ada berkas yang diupload";
-        }
+        $request->validate([
+            'berkas'=>'required|file|image|max:500',]);
+            echo $request->berkas->getClientOriginalName(). "lolos validasi";
     }
 }
